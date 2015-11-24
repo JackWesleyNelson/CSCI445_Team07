b8e54423c40af7c1f9d11ef0e1a1f0886bad78d5
Jack Nelson <janelson@mymail.mines.edu>
2015-11-23 20:03:49 -0700

    pushing the welcome page for laravel

--- Project_2/Unit2_Team/database/seeds/DatabaseSeeder.php
+++ Project_2/Unit2_Team/database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use App\Book;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +13,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Model::unguard();
+
         // $this->call(UserTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+        Model::reguard();
+    }
+}
+class BooksTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('books')->delete();
+
+        Book::create(['title' => 'A Tale of Two Cities', 'author' => 'Charles Dickens', 'year' => '1859']);
     }
 }
